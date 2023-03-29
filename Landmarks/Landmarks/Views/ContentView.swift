d45048889c67a50e4e4a704f3269962383140e2f
suyeon <rlatndus9911@naver.com>
2023-03-29 20:56:56 +0900

    SwiftUI Essentials - Building Lists and Navigation

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 김수연 on 2023/03/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
